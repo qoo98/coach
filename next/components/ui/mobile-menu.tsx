1bd9061689a2f72db3310cfa3ebd29fa226f39cd
qoo98 <chopper1224110@gmail.com>
2024-02-16 23:21:23 +0900

    dd

--- next/components/ui/mobile-menu.tsx
+++ next/components/ui/mobile-menu.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
+import { useState, useRef, useEffect } from 'react'
 
 export default function MobileMenu() {
   const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false)
@@ -12,10 +12,15 @@ export default function MobileMenu() {
   // close the mobile menu on click outside
   useEffect(() => {
     const clickHandler = ({ target }: { target: EventTarget | null }): void => {
-      if (!mobileNav.current || !trigger.current) return;
-      if (!mobileNavOpen || mobileNav.current.contains(target as Node) || trigger.current.contains(target as Node)) return;
+      if (!mobileNav.current || !trigger.current) return
+      if (
+        !mobileNavOpen ||
+        mobileNav.current.contains(target as Node) ||
+        trigger.current.contains(target as Node)
+      )
+        return
       setMobileNavOpen(false)
-    };
+    }
     document.addEventListener('click', clickHandler)
     return () => document.removeEventListener('click', clickHandler)
   })
@@ -23,9 +28,9 @@ export default function MobileMenu() {
   // close the mobile menu if the esc key is pressed
   useEffect(() => {
     const keyHandler = ({ keyCode }: { keyCode: number }): void => {
-      if (!mobileNavOpen || keyCode !== 27) return;
+      if (!mobileNavOpen || keyCode !== 27) return
       setMobileNavOpen(false)
-    };
+    }
     document.addEventListener('keydown', keyHandler)
     return () => document.removeEventListener('keydown', keyHandler)
   })
@@ -57,18 +62,27 @@ export default function MobileMenu() {
         id="mobile-nav"
         ref={mobileNav}
         className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out"
-        style={mobileNavOpen ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: 0.8 }}
+        style={
+          mobileNavOpen
+            ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 }
+            : { maxHeight: 0, opacity: 0.8 }
+        }
       >
         <ul className="bg-gray-800 px-4 py-2">
           <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
+            <Link
+              href="/signin"
+              className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center"
+              onClick={() => setMobileNavOpen(false)}
+            >
               Sign in
             </Link>
           </li>
           <li>
             <Link
               href="/signup"
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
+              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out"
+              onClick={() => setMobileNavOpen(false)}
             >
               Sign up
             </Link>
